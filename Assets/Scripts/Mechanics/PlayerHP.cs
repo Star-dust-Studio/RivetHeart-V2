60f92406600c1ca221a05f64461e5a30d5b11bae
vivianng1998 <69830300+vivianng1998@users.noreply.github.com>
2020-12-12 09:07:18 +0800

    all scripts

---
+++ Assets/Scripts/Mechanics/PlayerHP.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour
+{
+    public GameObject[] hpSprite;
+
+    private int maxHealth;
+    private int currentHealth;
+
+    // Spaghetti respawn point
+    public Transform respawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 5;
+        currentHealth = maxHealth;
+    }
+
+    public void MinusPlayerHP(int value)
+    {
+        currentHealth -= value;
+        //if (value >= 2)
+        //{
+        //    for (int i = currentHealth; i < maxHealth; i++)
+        //    {
+        //        hpSprite[i + 1].SetActive(false);
+        //    }
+        //}
+        //else
+        //{
+        //    hpSprite[currentHealth + 1].SetActive(false);
+        //}
+        Debug.Log(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Debug.Log("dead");
+            GetComponent<PlayerMovement>().playerState = 2;
+            StartCoroutine(Respawn());
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(1.5f);
+        transform.position = respawnPoint.position; // Spaghetti code
+        FullHeal();
+        GetComponent<PlayerMovement>().playerState = 0;
+    }
+
+    private void FullHeal()
+    {
+        currentHealth = maxHealth;
+        //for (int i = 0; i < maxHealth; i++)
+        //{
+        //    hpSprite[i].SetActive(true);
+        //}
+    }
+}
