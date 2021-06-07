02d40cb9d7356b5c3e196c24d2a5fcc254dc8749
vivianng1998 <69830300+vivianng1998@users.noreply.github.com>
2021-06-08 02:51:16 +0800

    save load system

---
+++ Assets/Scripts/Testing/TestData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TestData
+{
+    public int testAge;
+    public string testName;
+
+    public TestData(Data data)
+    {
+        testAge = data.age;
+        testName = data.myName;
+    }
+}
