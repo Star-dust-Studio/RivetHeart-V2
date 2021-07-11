a0db8af706feece58656ae4984ab68a826013d4c
vivianng1998 <69830300+vivianng1998@users.noreply.github.com>
2021-07-12 00:10:05 +0800

    alpha grapple

---
+++ Assets/Scripts/Testing/GrappleController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleController : MonoBehaviour
+{
+    public SlingGun slingGun;
+    public GrapplingHook grapplingHook;
+    public GameObject slingGunObject;
+    public GameObject grappleGunObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slingGun.enabled = true;
+        grapplingHook.enabled = false;
+        slingGunObject.SetActive(true);
+        grappleGunObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            slingGun.enabled = false;
+            grapplingHook.enabled = true;
+            slingGunObject.SetActive(false);
+            grappleGunObject.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            slingGun.enabled = true;
+            grapplingHook.enabled = false;
+            slingGunObject.SetActive(true);
+            grappleGunObject.SetActive(false);
+        }
+    }
+}
