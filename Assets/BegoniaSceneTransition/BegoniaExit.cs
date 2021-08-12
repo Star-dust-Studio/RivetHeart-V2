7df264d671ff020f8b004ecf5ed79ba0f36ba852
begoniasonata <cheongkhosin@gmail.com>
2021-08-12 21:49:08 +0800

    sceneTransition

---
+++ Assets/BegoniaSceneTransition/BegoniaExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BegoniaExit : MonoBehaviour
+{
+    public string sceneName;
+    [SerializeField]private string newScenePassword;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag =="Player")
+        {
+            BegoniaPlayerMovement.instance.scenePassword = newScenePassword;
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/BegoniaSceneTransition/BegoniaVectorValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Vector Value", menuName = "Vector Value")]
+public class BegoniaVectorValue : ScriptableObject
+{
+    public Vector2 initialValue;
+}
