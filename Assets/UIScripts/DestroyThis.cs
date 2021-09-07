151ed8ee0a22a76b2c7775743e77b27d5b3141ab
begoniasonata <cheongkhosin@gmail.com>
2021-09-08 03:36:45 +0800

    LowAmbience

---
+++ Assets/UIScripts/DestroyThis.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyThis : MonoBehaviour
+{
+    private AsyncOperation operation;
+
+    private void Awake()
+    {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        foreach (GameObject MusicBgm in musicObj)
+        {
+            GameObject.Destroy(MusicBgm);
+        }
+
+    }
+}
--- Assets/UIScripts/DontDestroyGameControl.cs
+++ Assets/UIScripts/DontDestroyGameControl.cs
@@ -7,9 +7,55 @@ public class DontDestroyGameControl : MonoBehaviour
     private AsyncOperation operation;
     private Canvas BGameCanvas;
 
+    //public GameObject CreditBgm;
+
+
     private void Awake()
     {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameObject[] MainAmObj = GameObject.FindGameObjectsWithTag("MainAm");
+        if (MainAmObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameObject[] LowAmObj = GameObject.FindGameObjectsWithTag("LowAm");
+        if (LowAmObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameObject[] CatAmObj = GameObject.FindGameObjectsWithTag("CatAm");
+        if (CatAmObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameObject[] GreenAmObj = GameObject.FindGameObjectsWithTag("GreenAm");
+        if (GreenAmObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameObject[] HighAmObj = GameObject.FindGameObjectsWithTag("HighAm");
+        if (HighAmObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameObject[] CaptAmObj = GameObject.FindGameObjectsWithTag("CaptAm");
+        if (CaptAmObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
         BGameCanvas = GetComponentInChildren<Canvas>(true);
         DontDestroyOnLoad(gameObject);
     }
+
 }
