01b2d5b13154f172c286842d958a71b61cdea158
begoniasonata <cheongkhosin@gmail.com>
2021-09-07 22:55:02 +0800

    dont destroy
    for game canvas

---
+++ Assets/UIScripts/DontDestroyGameControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyGameControl : MonoBehaviour
+{
+    private AsyncOperation operation;
+    private Canvas BGameCanvas;
+
+    private void Awake()
+    {
+        BGameCanvas = GetComponentInChildren<Canvas>(true);
+        DontDestroyOnLoad(gameObject);
+    }
+}
